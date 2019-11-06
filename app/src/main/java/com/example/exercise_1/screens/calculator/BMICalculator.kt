7768cd7bf6761da72f0aa4a792cfa4fab59cf296
Dominik Heiny <dominik-heiny@web.de>
2019-11-06 15:55:28 +0100

    - SafeArguments hinzugefügt

--- app/src/main/java/com/example/exercise_1/screens/calculator/BMICalculator.kt
+++ app/src/main/java/com/example/exercise_1/screens/calculator/BMICalculator.kt
@@ -1,26 +1,40 @@
 package com.example.exercise_1.screens.calculator
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.exercise_1.R
-import com.example.exercise_1.databinding.FragmentBmiCalculatorBinding
+import com.example.exercise_1.databinding.FragmentBmiCalculateBinding
 
 
 class BMICalculator : Fragment() {
 
+    private lateinit var viewModel : BMICalculatorViewModel
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        var binding: FragmentBmiCalculatorBinding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_bmi__calculator, container,  false)
+        var binding: FragmentBmiCalculateBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_bmi__calculate, container,  false)
 
-        binding.calculateButton.setOnClickListener(
-            Navigation.createNavigateOnClickListener(R.id.action_BMI_Calculator_to_BMIGoodResult)
-        )
+
+
+
+        viewModel = ViewModelProviders.of(this).get(BMICalculatorViewModel::class.java)
+
+        binding.calculateButton.setOnClickListener {view : View ->
+            val weightInput : String = binding.weightInput.text.toString()
+            val heihgtInput : String = binding.heightInput.text.toString()
+            var bmi =  viewModel.onCalculate(weightInput.toDouble(), heihgtInput.toDouble())
+            view.findNavController().navigate(BMICalculatorDirections.actionBMICalculatorToBMIGoodResult(bmi))
+
+        }
         return binding.root
     }
 }
--- app/src/main/java/com/example/exercise_1/screens/result/BMIGoodResult.kt
+++ app/src/main/java/com/example/exercise_1/screens/result/BMIGoodResult.kt
@@ -5,15 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.exercise_1.R
+import com.example.exercise_1.databinding.FragmentBmiGoodResultsBinding
 
 
 class BMIGoodResult : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bmi_good_result, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        var binding: FragmentBmiGoodResultsBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_bmi_good_results, container,  false)
+
+        val args = BMIGoodResultArgs.fromBundle(arguments!!)
+        val result = args.bmiResult
+
+        return binding.root
+        }
     }
-}
